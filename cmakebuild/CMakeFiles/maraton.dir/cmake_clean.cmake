317a70c059a2a52572fdae24e0c9c4e960f07269
shuboyang <shuboyang@yhgenomics.com>
2015-10-26 19:52:36 +0800

    rm mian.cpp

---
+++ cmakebuild/CMakeFiles/maraton.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/ide/main.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/msg/Message.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/ClusterSession.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/Executor.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/ExecutorManager.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/ExecutorSession.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/HTTPHandler.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/HTTPSession.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/Master.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/MasterSession.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/Session.o"
+  "CMakeFiles/maraton.dir/data/project/maraton-cli/maraton-framework/src/UVSockService.o"
+  "maraton.pdb"
+  "maraton"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/maraton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
