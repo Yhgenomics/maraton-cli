eb110033de870d4f9cbe2924336c1708a55d681d
BigCatRoyalYang <royalyk@126.com>
2015-11-11 11:45:55 +0800

    compile and link OK in ubuntu

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf cmakebuild/
-CMaker project:maraton-cli head:*.h,*.hpp src:*.cpp,*.hpp,*.cc,*.c flag:-Wall-std=c++11-pthread lib:lib/libuv.a,lib/maraton-framework.a,lib/libcurl.a
+CMaker project:maraton-cli head:*.h,*.hpp src:*.cpp,*.hpp,*.cc,*.c flag:-Wall-std=c++11-pthread-DCURL_STATICLIB lib:lib/libuv.a,lib/maraton-framework.a,lib/libcurl.a
 cd cmakebuild
 cmake .
 make -j 3
