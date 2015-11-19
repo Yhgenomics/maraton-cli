5151e3d30dca88749c6d479ecc2e45117b260bb4
BigCatRoyalYang <royalyk@126.com>
2015-11-19 15:57:01 +0800

    fix the tab problem

--- include/FileUploader.h
+++ include/FileUploader.h
@@ -6,29 +6,28 @@
 
 class FileUploader
 {
-public:	
-	bool	UploadFileViaHttp( std::string taskID , std::string fileFullName , std::string postDest );
+public:
+    bool    UploadFileViaHttp( std::string taskID , std::string fileFullName , std::string postDest );
 
 private:
-	bool	Init();
-	void	PreparePostMark( std::string taskID , std::string fileFullName );
-	void	SetOptions( std::string postDest );
-	void	PostFile();
-	void	Deinit();
-	
-	// set -1 for no waiting
-	void	SetTimeOut(long milliseconds );
+    bool    Init();
+    void    PreparePostMark( std::string taskID , std::string fileFullName );
+    void    SetOptions( std::string postDest );
+    void    PostFile();
+    void    Deinit();
 
-	CURL			*curl_;
-	CURLM			*multi_handle_;
-	curl_httppost	*post_;
-	curl_httppost	*last_;
-	curl_slist		*header_list_;
-	std::string		expect_;
-	int				running_flag;
-	timeval			timeout_;
-	long			timeout_ms_;
+    // set -1 for no waiting
+    void    SetTimeOut( long milliseconds );
 
+    CURL            *curl_;
+    CURLM           *multi_handle_;
+    curl_httppost   *post_;
+    curl_httppost   *last_;
+    curl_slist      *header_list_;
+    std::string     expect_;
+    int             running_flag;
+    timeval         timeout_;
+    long            timeout_ms_;
 };
 
 #endif 
