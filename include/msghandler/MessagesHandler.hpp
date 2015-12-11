48fb8e6cf739ec084bdcb2604aad1e15cd7ddd26
BigCatRoyalYang <royalyk@126.com>
2015-12-11 11:45:56 +0800

    add merge ,adjust taskdeliver and task result

--- include/msghandler/MessagesHandler.hpp
+++ include/msghandler/MessagesHandler.hpp
@@ -2,13 +2,20 @@
 #define MESSAGES_HANDLER_HPP_
 
 #include "stdlib.h"
+#include "HTTPMessageCommandHandler.hpp"
+#include "HTTPMessageListExecutorHandler.hpp"
+#include "HTTPMessageResultHandler.hpp"
+#include "HTTPMessageTaskStartHandler.hpp"
+#include "HTTPMessageTaskStopHandler.hpp"
 #include "MessageAbilityQueryHandler.hpp"
 #include "MessageAbilityQueryACKHandler.hpp"
 #include "MessageCommandHandler.hpp"
+#include "MessageConfigHandler.hpp"
 #include "MessageExceptionReportHandler.hpp"
 #include "MessageGreetingHandler.hpp"
 #include "MessageHeartBeatHandler.hpp"
 #include "MessageIntroHandler.hpp"
+#include "MessageMergeProcessHandler.hpp"
 #include "MessageResourcesQueryHandler.hpp"
 #include "MessageResourcesQueryACKHandler.hpp"
 #include "MessageStatusReportHandler.hpp"
@@ -29,7 +36,7 @@ namespace Protocol
     class MessagesHandler
     {
     public:
-
+    
         static int process( Message* message )
         {
             switch ( message->command() )
@@ -37,91 +44,99 @@ namespace Protocol
                 case 101:
                     return MessageAbilityQueryHandler( MessageAbilityQuery( message ) );
                     break;
-
+                
                 case 102:
                     return MessageAbilityQueryACKHandler( MessageAbilityQueryACK( message ) );
                     break;
-
+                
                 case 166:
                     return MessageCommandHandler( MessageCommand( message ) );
                     break;
-
+                
+                case 170:
+                    return MessageConfigHandler( MessageConfig( message ) );
+                    break;
+                
                 case 151:
                     return MessageExceptionReportHandler( MessageExceptionReport( message ) );
                     break;
-
+                
                 case 91:
                     return MessageGreetingHandler( MessageGreeting( message ) );
                     break;
-
+                
                 case 161:
                     return MessageHeartBeatHandler( MessageHeartBeat( message ) );
                     break;
-
+                
                 case 92:
                     return MessageIntroHandler( MessageIntro( message ) );
                     break;
-
+                
+                case 171:
+                    return MessageMergeProcessHandler( MessageMergeProcess( message ) );
+                    break;
+                
                 case 111:
                     return MessageResourcesQueryHandler( MessageResourcesQuery( message ) );
                     break;
-
+                
                 case 112:
                     return MessageResourcesQueryACKHandler( MessageResourcesQueryACK( message ) );
                     break;
-
+                
                 case 141:
                     return MessageStatusReportHandler( MessageStatusReport( message ) );
                     break;
-
+                
                 case 144:
                     return MessageTaskCancelHandler( MessageTaskCancel( message ) );
                     break;
-
+                
                 case 143:
                     return MessageTaskContinueHandler( MessageTaskContinue( message ) );
                     break;
-
+                
                 case 131:
                     return MessageTaskDeliverHandler( MessageTaskDeliver( message ) );
                     break;
-
+                
                 case 132:
                     return MessageTaskDeliverACKHandler( MessageTaskDeliverACK( message ) );
                     break;
-
+                
                 case 142:
                     return MessageTaskPauseHandler( MessageTaskPause( message ) );
                     break;
-
+                
                 case 134:
                     return MessageTaskProgressHandler( MessageTaskProgress( message ) );
                     break;
-
+                
                 case 121:
                     return MessageTaskQueryHandler( MessageTaskQuery( message ) );
                     break;
-
+                
                 case 122:
                     return MessageTaskQueryACKHandler( MessageTaskQueryACK( message ) );
                     break;
-
+                
                 case 145:
                     return MessageTaskRestartHandler( MessageTaskRestart( message ) );
                     break;
-
+                
                 case 133:
                     return MessageTaskResultHandler( MessageTaskResult( message ) );
                     break;
-
+                
                 default:
                     return -1;
                     break;
             } // End of switch
-
+            
         } // End of static int process
-
+        
     }; // End of class define of MessagesHandler
-
+    
 } // End of namespace Protocol
 #endif
